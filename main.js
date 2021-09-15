b19b849cd2db5ad1a83865f90ea9e70f5500dbf2
JaiDewan <85880546+JaiDewan@users.noreply.github.com>
2021-09-15 17:48:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+rover_width=100;
+rover_height=100;
+rover_x=10;
+rover_y=10;
+background_image="mars.pia01492-main.jpg";
+rover_image="rover.png";
+function add() {
+    background_imgTag=new Image();
+  background_imgTag.onload=uploadBackground;
+  background_imgTag.src=background_image; 
+  rover_imgTag=new Image();
+  rover_imgTag.onload=uploadrover;
+  rover_imgTag.src=rover_image;   
+}
+function uploadBackground(){
+ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.rover_height);
+
+}
+function uploadrover(){
+    ctx.drawImage(rover_imgTag,0,0,rover_width,rover_height);
+    
+    }
